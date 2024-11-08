9665719f69542ae316b7e8db11d326ec1dc61668
stefanopradella <stefano.pradella.97@gmail.com>
2024-11-08 21:42:42 +0100

    Modify scripts to compile into elf format
    Update "compileSW" to compile in elf format using linker script.
    Update "SimuRISC_Envinronment"  to include default linker script.

--- SimuRISC_Environment.m
+++ SimuRISC_Environment.m
@@ -2,5 +2,6 @@ classdef SimuRISC_Environment
     properties ( Constant = true )
         RISCV_COMPILER_PATH = '/home/stefano/riscv/bin';                    % This is where the assembler is installed
         VIVADO_PATH = '';
+        DEFAULT_LINKER_FILE = 'link.ld';
     end
 end
--- scripts/compileSW.m
+++ scripts/compileSW.m
@@ -1,30 +1,36 @@
-function compileSW(varargin)
-% COMPILESW - Compiles the assembler code into binary code.
-%
-%   COMPILESW() Prompts the user for thesource file location
-%   COMPILESW(filepath) Compiles the file specified in the filepath variable
+function compileSW(NameValueArgs)
+% COMPILESW - Compiles the assembler code into ELF file.
+%   Syntax:
+%       COMPILESW()                                 [Prompts the user for the source file location]
+%       COMPILESW('SourceFilePath', 'test.S')       [Uses default linker set in SimuRISC_Environment constants]
+%       COMPILESW('SourceFilePath', 'test.S', 'LinkerFilePath' 'myLinker.ld')
+    arguments
+        NameValueArgs.SourceFilePath (1,1) string = ''
+        NameValueArgs.LinkerFilePath (1,1) string = which(SimuRISC_Environment.DEFAULT_LINKER_FILE)
+    end
 
-    if nargin == 0
+    if strcmp(NameValueArgs.SourceFilePath, "")
         [filename, filepath] = uigetfile('*');
         sourceFilePath = [filepath, filename];
     else
-        sourceFilePath = varargin{1};
+        sourceFilePath = NameValueArgs.SourceFilePath;
     end
+    linkerFilePath = NameValueArgs.LinkerFilePath;
 
     [filePath,fileName,fileExtension]=fileparts(sourceFilePath);
 
-    sourceFilePath = [filePath filesep fileName fileExtension];
-    objectFilePath = [filePath filesep fileName '.o'];
-    binaryFilePath = [filePath filesep fileName '.bin'];
+    sourceFilePath = strcat(filePath, filesep, fileName, fileExtension);
+    objectFilePath = strcat(filePath, filesep, fileName, '.o');
+    elfFilePath = strcat(filePath, filesep, fileName, '.elf');
 
 
-    [status,cmdout] = system(['riscv32-unknown-elf-as -o ' objectFilePath ' ' sourceFilePath]);
+    [status,cmdout] = system(strcat("riscv32-unknown-elf-as -o ", objectFilePath," ", sourceFilePath));
     if status ~= 0
         ME = MException('compileSW:assemblerCommandError', ...
             'Error invoking assembler command: %s',cmdout);
     throw(ME)
     end
-    [status,cmdout] = system(['riscv32-unknown-elf-objcopy -O binary ' objectFilePath ' ' binaryFilePath]);
+    [status,cmdout] = system(strcat("riscv32-unknown-elf-ld -o ", elfFilePath, " -T ", linkerFilePath, " -m elf32lriscv -nostdlib --no-relax ", objectFilePath));
     if status ~= 0
         ME = MException('compileSW:objcopyCommandError', ...
             'Error invoking objcopy command: %s',cmdout);
