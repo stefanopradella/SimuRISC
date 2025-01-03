a30a7776aeed0421e75c1ae55dfc5f25f9573ce3
stefanopradella <stefano.pradella.97@gmail.com>
2025-01-03 10:15:59 +0100

    Add project version to Constants

--- SimuRISC_Constants.m
+++ SimuRISC_Constants.m
@@ -1,5 +1,6 @@
 classdef SimuRISC_Constants
     properties ( Constant = true )
+        VERSION                     =   '0.1.0';
         XLEN                        =   32;
         ADDR_BUS_WIDTH              =   16;
         DATATYPE_MEMORY_ADDR        =   fixdt(0,SimuRISC_Constants.ADDR_BUS_WIDTH-log2(SimuRISC_Constants.XLEN/8),0);
