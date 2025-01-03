835fe0568b1c171469988c7e7ae08342f51764d7
stefanopradella <stefano.pradella.97@gmail.com>
2025-01-03 10:05:51 +0100

    Update compiler and simulator path
    Update the environment constants class to use relative home path

--- SimuRISC_Environment.m
+++ SimuRISC_Environment.m
@@ -1,8 +1,8 @@
 classdef SimuRISC_Environment
     properties ( Constant = true )
-        RISCV_COMPILER_PATH = '/home/stefano/riscv/bin';                    % Location of the compiler binary
-        RISCV_SIMULATOR_PATH = '/home/stefano/riscv/whisper';               % Location of the whisper binary
-        VIVADO_PATH = '';
+        RISCV_COMPILER_PATH = '~/riscv/bin';                    % Location of the compiler binary
+        RISCV_SIMULATOR_PATH = '~/riscv/whisper';               % Location of the whisper binary
+        VIVADO_PATH = '/opt/Xilinx/Vivado/2024.2/bin';
         DEFAULT_LINKER_FILE = 'link.ld';
         DEFAULT_WHISPER_CONFIG_FILE = 'whisperConfiguration.cfg'
     end
