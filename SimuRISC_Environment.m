1aac5a7d10f5d1cb19a2b9ab30242b3930e868c1
stefanopradella <stefano.pradella.97@gmail.com>
2025-06-02 19:38:53 +0200

    Add path for riscv-tests folder

--- SimuRISC_Environment.m
+++ SimuRISC_Environment.m
@@ -2,6 +2,7 @@ classdef SimuRISC_Environment
     properties ( Constant = true )
         RISCV_COMPILER_PATH = '~/riscv/bin';                    % Location of the compiler binary
         RISCV_SIMULATOR_PATH = '~/riscv/whisper';               % Location of the whisper binary
+        RISCV_TESTS_PATH = '~/riscv/riscv-tests';               % Location of the riscv-tests folder
         VIVADO_PATH = '/opt/Xilinx/Vivado/2024.2/bin';
         DEFAULT_LINKER_FILE = 'link.ld';
         DEFAULT_WHISPER_CONFIG_FILE = 'whisperConfiguration.cfg'
