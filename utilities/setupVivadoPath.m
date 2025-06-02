e412c527a26c2eb661f6f1eb527c9d07bca84252
stefanopradella <stefano.pradella.97@gmail.com>
2025-06-02 19:38:54 +0200

    Handle error when Vivado is not found in path

--- utilities/setupVivadoPath.m
+++ utilities/setupVivadoPath.m
@@ -1,5 +1,7 @@
-try
-    hdlsetuptoolpath('ToolName','Xilinx Vivado','ToolPath', SimuRISC_Environment.VIVADO_PATH);
-catch
-    error('Error setting up Vivado path. Make sure Vivado is installed and the correct path is set into SimuRISC_Environment. Then run this script again.')
+if isdir(SimuRISC_Environment.VIVADO_PATH)
+    try
+        hdlsetuptoolpath('ToolName','Xilinx Vivado','ToolPath', SimuRISC_Environment.VIVADO_PATH);
+    catch
+        error('Error setting up Vivado path. Make sure Vivado is installed and the correct path is set into SimuRISC_Environment. Then run this script again.')
+    end
 end
