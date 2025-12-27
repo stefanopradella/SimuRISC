32461e6f9dc60de31bca6dbf5c1cf8c41811ba79
stefanopradella <stefano.pradella.97@gmail.com>
2025-12-27 11:22:43 +0100

    Convert runBuiltinTests to a MATLAB test class

---
+++ tests/RunBuiltinTests.m
@@ -0,0 +1,43 @@
+classdef RunBuiltinTests < matlab.unittest.TestCase
+    properties (MethodSetupParameter)
+        testName = {"cleanExit", ...
+                    "memoryReadWrite", ...
+                    "subset_v0_2_part3", ...
+                    "subset_v0_2_part2", ...
+                    "subset_v0_2_part1", ...
+                    "subset_v0_1", ...
+                    "subset_v0_3", ...
+        
+            };
+    end
+
+    properties
+        elfFilePath;
+    end
+
+    methods (TestMethodSetup)
+        function compileCode(testCase, testName)
+            testFolderPath = strcat(getProjectRoot(), filesep, "tests", filesep, testName);
+            sourceFilePath = testFolderPath+filesep+testName+".s";
+            testCase.elfFilePath = testFolderPath+filesep+testName+".elf";
+
+            % Compile test code
+            compileSW('SourceFilePath', sourceFilePath);
+        end
+    end
+
+    methods (Test)
+        function testExecution(testCase)
+
+            % Run test on model
+            modelOutput = runTestCase("ElfFilePath", testCase.elfFilePath);
+        
+            % Simulate test and get RAM dump
+            simulationOutput = whisperSimulateElf("ElfFilePath", testCase.elfFilePath);
+        
+            % Check results
+            testCase.verifyEqual(simulationOutput.dataMemory, modelOutput.dataMemory)
+            testCase.verifyEqual(simulationOutput.registerFile, modelOutput.registerFile)
+        end
+    end
+end
