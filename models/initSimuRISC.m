9577d761c29bae4fa14bc3cb8d6214df0d8528a0
stefanopradella <stefano.pradella.97@gmail.com>
2025-12-27 10:49:12 +0100

    Edit models
    Fix a bug in branch instruction handling.
    Add a switch for a stop condition when decoding ecall.
    Setting simulation instruction limit in init script.

--- models/initSimuRISC.m
+++ models/initSimuRISC.m
@@ -3,9 +3,17 @@ registerDataType                =   ['uint' num2str(SimuRISC_Constants.XLEN)];
 addrTranslationMask             =   bitcmp(SimuRISC_Constants.RAM_BASE_ADDR, registerDataType);
 
 clockFrequency                  =   45e6;
-modelStopCondition              =   "tohost";
 
-enableCSR = false;
+if ~exist('modelStopCondition', 'var')
+    modelStopCondition          =   "tohost";
+end
+
+if ~exist('enableCSR', 'var')
+    enableCSR                   =   false;
+end
+
+
+
 %% Initialize CSR
 CSRInitialValue = int32(zeros(size(SimuRISC_Constants.CSR_LUT, 1),1));
 valid_csrs = uint32(SimuRISC_Constants.CSR_LUT(:, 1));
@@ -14,3 +22,7 @@ valid_csrs = uint32(SimuRISC_Constants.CSR_LUT(:, 1));
 misaValue = uint32(0);
 
 CSRInitialValue(SimuRISC_Constants.CSR_LUT(:, 1) == hex2dec('300'))         =   hex2dec('0x1800');      %  Only m-mode is supported
+
+%% Simulation time limit
+
+simMaxTime                      =   1e3/clockFrequency;
