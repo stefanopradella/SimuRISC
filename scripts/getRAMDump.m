12ed10085e14196acac7f4fdce29063ae68a62e7
stefanopradella <stefano.pradella.97@gmail.com>
2025-12-31 11:49:40 +0100

    Edit getRAMDump to work with column addressing

--- scripts/getRAMDump.m
+++ scripts/getRAMDump.m
@@ -1,11 +1,17 @@
 function RAMDump = getRAMDump(simulationOutput, RAMinitialValue)
     RAMDump = RAMinitialValue;
-    dataMemory_wr_addr = squeeze(find(simulationOutput.logsout, 'dataMemory_wr_addr').Values.Data);
-    dataMemory_din = squeeze(find(simulationOutput.logsout, 'dataMemory_din').Values.Data);
+    dataMemory_wr_addr = squeeze(find(simulationOutput.logsout, 'dataMemory_addr').Values.Data);
+    dataMemory_din = fi(squeeze(find(simulationOutput.logsout, 'dataMemory_din').Values.Data), 0, 32, 0);   % TODO parametric wlen for data
     dataMemory_we = squeeze(find(simulationOutput.logsout, 'dataMemory_we').Values.Data);
 
-    writeAddr = dataMemory_wr_addr(dataMemory_we);
-    writeData = dataMemory_din(dataMemory_we);
+    dataMemory_nColumns = dataMemory_we.WordLength;                    
+    dataMemory_columnSize = dataMemory_din.WordLength / dataMemory_nColumns;
 
-    RAMDump(writeAddr+1) = writeData;
+    dataMemory_we_mask = fi(zeros(size(dataMemory_we)), dataMemory_din.Signed, dataMemory_din.WordLength, dataMemory_din.FractionLength);
+    dataMemory_we_mask.bin = repelem(dataMemory_we.bin, 1, dataMemory_columnSize);
+
+    writeAddr = dataMemory_wr_addr(dataMemory_we>0);
+    writeData = bitand(dataMemory_din, dataMemory_we_mask);
+
+    RAMDump(writeAddr+1) = writeData(dataMemory_we>0);
 end
