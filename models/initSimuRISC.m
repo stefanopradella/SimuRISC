c9d075281417a764a0ce45c284d0a108a2f7859a
stefanopradella <stefano.pradella.97@gmail.com>
2026-01-03 18:55:13 +0100

    Edit memory for VHDL code generation
    Re-use the same logic both on code and data memory.
    Add code memory driver to prevent Vivado from deleting the block.

--- models/initSimuRISC.m
+++ models/initSimuRISC.m
@@ -1,7 +1,6 @@
 variant                         =   'RVI32';
 registerDataType                =   ['uint' num2str(SimuRISC_Constants.XLEN)];
 addrTranslationMask             =   bitcmp(SimuRISC_Constants.RAM_BASE_ADDR, registerDataType);
-
 clockFrequency                  =   45e6;
 
 if ~exist('modelStopCondition', 'var')
